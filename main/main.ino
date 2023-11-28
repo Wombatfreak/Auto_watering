56277aa63cd52681a0dd155e837cee07bcea1275
Wombatfreak <madsptak@gmail.com>
2023-11-28 18:35:39 +0100

    Final

--- main/main.ino
+++ main/main.ino
@@ -14,8 +14,11 @@ Servo myservo;
 int pos = 0;
 
 float water_level = 0;
+float light_level = 0;
 float humidity = 0;
 float temperature = 0;
+uint8_t tx_cnt = 0;
+uint8_t seq_old = 0;
 int wakeup_cnt = 0;
 unsigned long startTime;
 
@@ -23,6 +26,8 @@ void setup()
 {
 
   myservo.attach(7);
+  myservo.write(0); 
+  myservo.detach();
   // Set up Watchdog Timer
   MCUSR &= ~(1 << WDRF);          // Clear the reset flag
   WDTCSR |= (1 << WDCE) | (1 << WDE);  // Set up WDT for changes
@@ -33,8 +38,8 @@ void setup()
   Serial.begin(9600); //serial port to computer
   mySerial.begin(9600);
   Serial.println("Startup");
-  //initialize_radio();
-  //myLora.tx("TTN Mapper on TTN Enschede node");
+  initialize_radio();
+  myLora.tx("TTN Mapper on TTN Enschede node");
   delay(2000);
 }
 
@@ -93,52 +98,72 @@ void initialize_radio()
 
 void loop()
 {
-  if (wakeup_cnt >= 1) { //450 = 1 hour
+  Serial.println("Line: 99");
+  if (wakeup_cnt >= 2) { //450 = 1 hour
+    myLora.autobaud();
+    Serial.println("Line: 101");
     wakeup_cnt = 0;
+    Serial.println("Line: 103");
     water_level = analogRead(A0);
+    Serial.println("Line: 105");
+    light_level = analogRead(A1);
+    Serial.println("Line: 107");
+    delay(50);
     humidity = dht11.readHumidity();
+    Serial.println("Line: 109");
+    delay(50);
     temperature = dht11.readTemperature();
-    Serial.print("Water Level: ");
-    Serial.println(water_level);
-    Serial.print("Humidity: ");
-    Serial.println(humidity);
-    Serial.print("Temperature: ");
-    Serial.println(temperature);
     Serial.print("TXing");
-      //myLora.txCnf("!"); //one byte, blocking function
-      //switch(myLora.txCnf("!")) //one byte, blocking function
-      //{
-      //  case TX_FAIL:
-      //  {
-      //    Serial.println("TX unsuccessful or not acknowledged");
-      //    break;
-      //  }
-      //  case TX_SUCCESS:
-      //  {
-      //    Serial.println("TX successful and acknowledged");
-      //    break;
-      //  }
-      //  case TX_WITH_RX:
-      //  {
-      //    String received = myLora.getRx();
-      //    received = myLora.base16decode(received);
-      //    Serial.print("Received downlink: " + received);
-      //    break;
-      //  }
-      //  default:
-      //  {
-      //    Serial.println("Unknown response from TX function");
-      //  }
-      //}
-    if (humidity < 38 && temperature > 0){
+    uint8_t txBuffer[] = {water_level, humidity, temperature, light_level, tx_cnt};
+    myLora.txBytes(txBuffer,sizeof(txBuffer));
+    Serial.print("Bytes: ");
+    Serial.println(sizeof(txBuffer));
+    delay(50);
+    String data = myLora.getRx();
+
+    uint8_t numbers[3]; // Array to store the numbers
+
+    // Splitting the string and converting to integer
+    numbers[0] = (uint8_t)strtol(data.substring(0, 2).c_str(), NULL, 16);
+    numbers[1] = (uint8_t)strtol(data.substring(2, 4).c_str(), NULL, 16);
+    numbers[2] = (uint8_t)strtol(data.substring(4, 6).c_str(), NULL, 16);
+
+    // Printing the numbers (optional, for verification)
+    Serial.print("Number 1: ");
+    Serial.println(numbers[0]);
+    Serial.print("Number 2: ");
+    Serial.println(numbers[1]);
+    Serial.println(tx_cnt);
+    Serial.print("Number 3: ");
+    Serial.println(numbers[2]);
+
+    //char data_buf[data.length()];
+    //data.toCharArray(data_buf,data.length());
+    //Serial.println(data);
+    //Serial.println("Data: ");
+    //Serial.println(data_buf[0]);
+    //int byte1 = int1*16+int2;
+    //Serial.println(data_buf[1]);
+    //Serial.println(data_buf[2]);
+    //Serial.println(data_buf[3]);
+    //Serial.println(byte1);
+//(humidity < 38 && temperature > 0) || 
+    if ((numbers[0] == 1 || numbers[0] == 2) && numbers[2] !=  seq_old){
+      myservo.attach(7);
       Serial.println("Opening...");
       myservo.write(90);              // tell servo to go to position in variable 'pos'
-      delay(3000);                       // waits 15ms for the servo to reach the position
+      unsigned long water_time = numbers[1]*1000;
+      delay(water_time);                       // waits 15ms for the servo to reach the position
       Serial.println("Closing...");
       myservo.write(0);              // tell servo to go to position in variable 'pos'
-      delay(500);                       // waits 15ms for the servo to reach the position
+      delay(90);                       // waits 15ms for the servo to reach the position
+      myservo.detach();
     }
+    seq_old = numbers[2];
+    tx_cnt = tx_cnt + 1; 
+    myLora.sleep(8000);
   }
+    Serial.println("Line: 158");
      wakeup_cnt = wakeup_cnt + 1;
      enterSleep();
 }
@@ -159,3 +184,29 @@ void enterSleep() {
     sleep_disable();
     Serial.println("Exiting sleep");
 }
+
+    //myLora.txCnf("!"); //one byte, blocking function
+    //switch(myLora.txCnf("!")) //one byte, blocking function
+    //{
+    //  case TX_FAIL:
+    //  {
+    //    Serial.println("TX unsuccessful or not acknowledged");
+    //    break;
+    //  }
+    //  case TX_SUCCESS:
+    //  {
+    //    Serial.println("TX successful and acknowledged");
+    //    break;
+    //  }
+    //  case TX_WITH_RX:
+    //  {
+    //    String received = myLora.getRx();
+    //    received = myLora.base16decode(received);
+    //    Serial.print("Received downlink: " + received);
+    //    break;
+    //  }
+    //  default:
+    //  {
+    //    Serial.println("Unknown response from TX function");
+    //  }
+    //}
